6ce526544322fc84ad412755b6d50d4c2cff1d8d
pizza <npwchr.work@gmail.com>
2022-04-12 14:15:58 +0700

    install: fontawesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import { Fragment } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: any) {
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+      <i className="far fa-address-book"></i>
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
