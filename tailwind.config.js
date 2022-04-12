373f40c72601248174c97a35cb198ae631b36ed1
pizza <npwchr.work@gmail.com>
2022-04-12 14:56:32 +0700

    config: css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          10: "#CDBFC4",
+          20: "#F8E8E6",
+          30: "#8E96AA",
+          40: "#5F5357",
+          50: "#3F3F40",
+        },
+        primary: {
+          10: "#168FBD",
+          20: "#45788D",
+          30: "#295B8D",
+          40: "#3B586A",
+        },
+        error: "#EF4923",
+      },
+    },
   },
   plugins: [],
 };
