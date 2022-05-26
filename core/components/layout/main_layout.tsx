df31fd4f48a4f1319236b81109929f54134bfe91
pizza <npwchr.work@gmail.com>
2022-05-27 00:38:23 +0700

    done: foother main layout

--- core/components/layout/main_layout.tsx
+++ core/components/layout/main_layout.tsx
@@ -32,13 +32,16 @@ export const MainLayout = (props: MainLayoutProps) => {
             className="relative w-full overflow-y-auto"
             style={{ height: "calc(100%)" }}
           >
-            <div className="min-h-[calc(100%-320px)] max-w-[1200px] mx-auto">
+            <div className="min-h-[calc(100%-160px)] laptop:min-h-[calc(100%-320px)] max-w-[1200px] mx-auto">
               {children}
             </div>
-            <div className="h-[320px] bg-dark-50 w-screen">
-              <div className="max-w-[1200px] mx-auto pt-[83px] pb-[31px] flex flex-col">
-                <div className="flex items-center justify-between">
-                  <img src="/images/logo_white.svg" className="h-[103px]" />
+            <div className="h-[160px] laptop:h-[320px] bg-dark-50 w-screen laptop:block flex items-center justify-center">
+              <div className="max-w-[1200px] w-full laptop:w-auto mx-auto laptop:pt-[83px] laptop:pb-[31px] flex flex-col justify-center laptop:justify-start">
+                <div className="flex items-center justify-between w-full laptop:w-auto laptop:p-0 px-[8px]">
+                  <img
+                    src="/images/logo_white.svg"
+                    className="h-[48px] laptop:h-[103px]"
+                  />
                   <div className="text-white">
                     <p className="topic2">Contact us</p>
                     <a
@@ -50,10 +53,8 @@ export const MainLayout = (props: MainLayoutProps) => {
                     </a>
                   </div>
                 </div>
-                <div className="h-[1px] bg-white w-full mt-[50px] mb-[28px]" />
-                <p className="text-white text-body">
-                  © 2022 All Right Reserved
-                </p>
+                <div className="h-[1px] bg-white w-full laptop:mt-[50px] laptop:mb-[28px] mt-[24px] mb-[16px]" />
+                <p className="text-white body">© 2022 All Right Reserved</p>
               </div>
             </div>
           </div>
