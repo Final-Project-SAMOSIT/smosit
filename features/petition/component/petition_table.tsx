d0675dc7d2983de845a29a31c1c240998ee9180c
pizza <npwchr.work@gmail.com>
2022-05-27 02:25:08 +0700

    done: petition page responsive

--- features/petition/component/petition_table.tsx
+++ features/petition/component/petition_table.tsx
@@ -37,7 +37,7 @@ export const PetitionTable = (props: PetitionTableProps) => {
           <p className="title pb-[15px] border-b border-black px-[12px]">
             Status
           </p>
-          <div className="flex flex-col w-[360px] justify-center space-y-[16px]">
+          <div className="flex flex-col laptop:w-[360px] w-[300px] justify-center space-y-[16px]">
             <div className="flex w-full">
               <div
                 className="flex justify-center w-1/2 cursor-pointer select-none"
@@ -52,12 +52,12 @@ export const PetitionTable = (props: PetitionTableProps) => {
                 <p className="heading6">completed</p>
               </div>
             </div>
-            <div className="w-[360px] h-[2px] bg-gray-20">
+            <div className="laptop:w-[360px] w-[300px] h-[2px] bg-gray-20">
               <div
                 className={classNames(
                   "w-1/2 h-full bg-gray-50 transition-all duration-200",
                   {
-                    "ml-[180px]": filterType === "Completed",
+                    "ml-[150px] laptop:ml-[180px]": filterType === "Completed",
                   }
                 )}
               />
@@ -67,25 +67,25 @@ export const PetitionTable = (props: PetitionTableProps) => {
           {getShowPetition().length === 0 ? (
             <div>no Data</div>
           ) : (
-            <div className="grid w-full grid-cols-10">
-              <div className="col-span-2 pb-[38px] col-start-2">
+            <div className="grid w-full grid-cols-6 laptop:grid-cols-10">
+              <div className="col-span-2 laptop:pb-[38px] pb-[24px] col-start-2 laptop:block hidden">
                 <p className="heading6">date</p>
               </div>
-              <div className="col-span-2 pb-[38px]">
+              <div className="laptop:col-span-2 col-span-3 laptop:pb-[38px] pb-[24px]">
                 <p className="heading6">topic</p>
               </div>
-              <div className="col-span-2 pb-[38px]">
+              <div className="col-span-2 laptop:pb-[38px] pb-[24px]">
                 <p className="heading6">type</p>
               </div>
-              <div className="col-span-2 pb-[38px]">
+              <div className="col-span-2 laptop:pb-[38px] pb-[24px] laptop:block hidden">
                 <p className="heading6">status</p>
               </div>
-              <div className="col-span-1 pb-[38px]" />
+              <div className="col-span-1 laptop:pb-[38px] pb-[24px]" />
               <div className="border-b border-gray-40 col-span-full" />
 
               {_.map(getShowPetition(), (petition) => (
                 <Fragment>
-                  <div className="col-span-2 col-start-2 min-h-[50px] flex items-center">
+                  <div className="col-span-2 col-start-2 min-h-[50px] laptop:flex items-center hidden">
                     <p className="body">
                       {dayjs(
                         petition.pet_date,
@@ -93,13 +93,13 @@ export const PetitionTable = (props: PetitionTableProps) => {
                       ).format("D/M/YYYY")}
                     </p>
                   </div>
-                  <div className="col-span-2 min-h-[50px] flex items-center">
+                  <div className="laptop:col-span-2 col-span-3 min-h-[50px] flex items-center">
                     <p className="body">{petition.pet_topic}</p>
                   </div>
                   <div className="col-span-2 min-h-[50px] flex items-center">
                     <p className="body">{petition.pet_types.pet_type_name}</p>
                   </div>
-                  <div className="col-span-2 min-h-[50px] flex items-center">
+                  <div className="col-span-2 min-h-[50px] items-center laptop:flex hidden">
                     <p className="body">{petition.status.status_name}</p>
                   </div>
                   <div className="col-span-1 min-h-[50px] flex items-center">
@@ -120,6 +120,7 @@ export const PetitionTable = (props: PetitionTableProps) => {
                     />
                   </div>
 
+                  {/* expand detail */}
                   <div
                     className={classNames(
                       "col-span-full overflow-y-hidden transition-all flex flex-col items-center justify-center duration-300 relative",
@@ -129,77 +130,158 @@ export const PetitionTable = (props: PetitionTableProps) => {
                       }
                     )}
                   >
-                    <div className="flex justify-center space-x-[25px] items-center relative">
-                      <div className="w-[76px] h-[76px] rounded-full bg-gray-30 flex items-end justify-center">
+                    <div className="flex justify-center space-x-[8px] laptop:space-x-[25px] items-center relative">
+                      <div className="laptop:w-[76px] laptop:h-[76px] h-[24px] w-[24px] rounded-full bg-gray-30 flex items-end justify-center">
                         <p className="bottom-[-28px] absolute body w-max ">
-                          Sent Form
+                          <span className="laptop:text-[14px] text-[12px]">
+                            Sent Form
+                          </span>
                         </p>
                         {Number(petition.status_id) >= 1 && (
                           <div className="flex items-center justify-center w-full h-full bg-black rounded-full">
-                            <i className="text-5xl text-white fas fa-check"></i>
+                            <i className="text-white laptop:text-5xl text-md fas fa-check"></i>
                           </div>
                         )}
                       </div>
                       <div
-                        className={classNames("w-[160px] h-[6px] bg-gray-30", {
-                          "bg-black": Number(petition.status_id) >= 2,
-                        })}
+                        className={classNames(
+                          "laptop:w-[160px] w-[24px] laptop:h-[6px] h-[3px] bg-gray-30",
+                          {
+                            "bg-black": Number(petition.status_id) >= 2,
+                          }
+                        )}
                       />
-                      <div className="w-[76px] h-[76px] rounded-full bg-gray-30 flex items-end justify-center relative">
-                        <p className="bottom-[-28px] absolute body w-max ">
-                          Approve / Reject
+                      <div className="laptop:w-[76px] laptop:h-[76px] h-[24px] w-[24px] rounded-full bg-gray-30 flex items-end justify-center relative">
+                        <p className="bottom-[-28px] absolute body w-max flex flex-col">
+                          {Number(petition.status_id) > 1 &&
+                            Number(petition.status_id) !== 5 && (
+                              <span className="laptop:text-[14px] text-[12px]">
+                                Approve
+                              </span>
+                            )}
+                          {Number(petition.status_id) === 5 && (
+                            <span className="laptop:text-[14px] text-[12px]">
+                              Reject
+                            </span>
+                          )}
+                          {Number(petition.status_id) === 1 && (
+                            <span className="laptop:text-[14px] text-[12px]">
+                              / Reject
+                            </span>
+                          )}
                         </p>
                         {Number(petition.status_id) >= 2 && (
                           <div className="flex items-center justify-center w-full h-full bg-black rounded-full">
-                            <i className="text-5xl text-white fas fa-check"></i>
+                            <i className="text-white laptop:text-5xl text-md fas fa-check"></i>
                           </div>
                         )}
                       </div>
                       <div
-                        className={classNames("w-[160px] h-[6px] bg-gray-30", {
-                          "bg-black":
-                            Number(petition.status_id) >= 3 &&
-                            Number(petition.status_id) !== 5,
-                        })}
+                        className={classNames(
+                          "laptop:w-[160px] w-[24px] laptop:h-[6px] h-[3px] bg-gray-30",
+                          {
+                            "bg-black":
+                              Number(petition.status_id) >= 3 &&
+                              Number(petition.status_id) !== 5,
+                          }
+                        )}
                       />
-                      <div className="w-[76px] h-[76px] rounded-full bg-gray-30 flex items-end justify-center relative">
+                      <div className="laptop:w-[76px] laptop:h-[76px] h-[24px] w-[24px] rounded-full bg-gray-30 flex items-end justify-center relative">
                         <p className="bottom-[-28px] absolute body w-max ">
-                          In Progress
+                          <span className="laptop:text-[14px] text-[12px]">
+                            In Progress
+                          </span>
                         </p>
                         {Number(petition.status_id) >= 3 &&
                           Number(petition.status_id) !== 5 && (
                             <div className="flex items-center justify-center w-full h-full bg-black rounded-full">
-                              <i className="text-5xl text-white fas fa-check"></i>
+                              <i className="text-white laptop:text-5xl text-md fas fa-check"></i>
                             </div>
                           )}
                       </div>
                       <div
-                        className={classNames("w-[160px] h-[6px] bg-gray-30", {
-                          "bg-black":
-                            Number(petition.status_id) >= 4 &&
-                            Number(petition.status_id) !== 5,
-                        })}
+                        className={classNames(
+                          "laptop:w-[160px] w-[24px] laptop:h-[6px] h-[3px] bg-gray-30",
+                          {
+                            "bg-black":
+                              Number(petition.status_id) >= 4 &&
+                              Number(petition.status_id) !== 5,
+                          }
+                        )}
                       />
-                      <div className="w-[76px] h-[76px] rounded-full bg-gray-30 flex items-end justify-center relative">
+                      <div className="laptop:w-[76px] laptop:h-[76px] h-[24px] w-[24px] rounded-full bg-gray-30 flex items-end justify-center relative">
                         <p className="bottom-[-28px] absolute body w-max ">
-                          Done
+                          <span className="laptop:text-[14px] text-[12px]">
+                            Done
+                          </span>
                         </p>
                         {Number(petition.status_id) >= 4 &&
                           Number(petition.status_id) !== 5 && (
                             <div className="flex items-center justify-center w-full h-full bg-black rounded-full">
-                              <i className="text-5xl text-white fas fa-check"></i>
+                              <i className="text-white laptop:text-5xl text-md fas fa-check"></i>
                             </div>
                           )}
                       </div>
                     </div>
                     <div className="grid w-full grid-cols-10">
                       <div className="mt-[48px] col-span-9 col-start-2 space-y-[8px]">
-                        <p className="caption1">
-                          Status: {petition.status.status_description}
-                        </p>
-                        <p className="caption1">
-                          Detail: {petition.pet_details}
-                        </p>
+                        <table className="">
+                          <tbody>
+                            <tr className="laptop:hidden">
+                              <td className="flex items-start">
+                                <p className="caption1">Topic:</p>
+                              </td>
+                              <td>
+                                <p className="caption1 pl-[8px]">
+                                  {petition.pet_topic}
+                                </p>
+                              </td>
+                            </tr>
+                            <tr className="laptop:hidden">
+                              <td className="flex items-start">
+                                <p className="caption1">Type:</p>
+                              </td>
+                              <td>
+                                <p className="caption1 pl-[8px]">
+                                  {petition.pet_types.pet_type_name}
+                                </p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className="flex items-start">
+                                <p className="caption1">Detail:</p>
+                              </td>
+                              <td>
+                                <p className="caption1 pl-[8px]">
+                                  {petition.pet_details}
+                                </p>
+                              </td>
+                            </tr>
+                            <tr className="laptop:hidden">
+                              <td className="flex items-start">
+                                <p className="caption1">Date:</p>
+                              </td>
+                              <td>
+                                <p className="caption1 pl-[8px]">
+                                  {dayjs(
+                                    petition.pet_date,
+                                    "YYYY-MM-DDTHH:mm:ss.SSSZ"
+                                  ).format("D/M/YYYY")}
+                                </p>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className="flex items-start">
+                                <p className="caption1">Status:</p>
+                              </td>
+                              <td>
+                                <p className="caption1 pl-[8px]">
+                                  {petition.status.status_description}
+                                </p>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
                       </div>
                     </div>
                   </div>
