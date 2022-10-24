132b48ba5dfc5aa50a4e0840d0ea0a1b393109f6
pizza <npwchr.work@gmail.com>
2022-10-24 15:57:59 +0700

    done: proposal PDF

--- features/document/components/proposal_pdf.tsx
+++ features/document/components/proposal_pdf.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { useTranslation } from "next-i18next";
 import { Observer } from "mobx-react-lite";
 import { FormikProps } from "formik";
@@ -6,6 +6,8 @@ import dayjs from "dayjs";
 import "dayjs/locale/th";
 import { documentProposalFormContext } from "../context/document_proposal_form.context";
 import { proposalInitvalue } from "../form/document_proposal.form";
+import classNames from "classnames";
+import _ from "lodash";
 
 interface ProposalPDFProps {
   formik: FormikProps<typeof proposalInitvalue>;
@@ -28,12 +30,27 @@ export const ProposalPDF = (props: ProposalPDFProps) => {
   const context = useContext(documentProposalFormContext);
 
   //---------------------
-  //   CONTEXT
+  //   HANDLED
+  //---------------------
+  function getDate() {
+    return "1-2";
+  }
+
+  function getMonth() {
+    return "ตุลาคม";
+  }
+
+  function getYear() {
+    return "2565";
+  }
+
+  //---------------------
+  //   EFFECT
   //---------------------
   useEffect(() => {
     setTimeout(() => {
-      window.print();
-      context.isPrinting = false;
+      // window.print();
+      // context.isPrinting = false;
     }, 300);
   }, []);
 
@@ -45,6 +62,101 @@ export const ProposalPDF = (props: ProposalPDFProps) => {
       {() => (
         <div className="relative text-[2.2vw] text-pdf font-medium">
           <img src="/images/proposal.jpg" className="w-full" alt="" />
+          <p className="absolute top-[7.5%] left-[10.8%] w-[13.5%] text-center">
+            {formik.values.form_info.form_no}
+          </p>
+          <p className="absolute top-[7.5%] left-[64.7%] w-[25.1%]">
+            {formik.values.form_info.form_no}
+          </p>
+          <p className="absolute top-[9.3%] left-[61%] w-[4.3%] text-center">
+            {dayjs(new Date()).locale("th").add(543, "year").format("D")}
+          </p>
+          <p className="absolute top-[9.3%] left-[69.5%] w-[11%] text-center">
+            {dayjs(new Date()).locale("th").add(543, "year").format("MMMM")}
+          </p>
+          <p className="absolute top-[9.3%] left-[83.6%] w-[6.6%] text-center">
+            {dayjs(new Date()).locale("th").add(543, "year").format("YYYY")}
+          </p>
+          <p className="absolute top-[11%] left-[26.5%] w-[27%]">
+            {formik.values.form_info.solution}
+          </p>
+          <p className="absolute top-[14.5%] left-[21.3%] w-[24%] text-center">
+            {formik.values.request_info.project_due_to}
+          </p>
+          <p className="absolute top-[14.5%] left-[53%] w-[33%] ">
+            {formik.values.request_info.project_name}
+          </p>
+          <p className="absolute top-[16.3%] left-[17.3%] w-[10.7%] text-center">
+            {getDate()}
+          </p>
+          <p className="absolute top-[16.3%] left-[32.4%] w-[12%] text-center">
+            {getMonth()}
+          </p>
+          <p className="absolute top-[16.3%] left-[48.2%] w-[7%] text-center">
+            {getYear()}
+          </p>
+          <p className="absolute top-[16.3%] left-[57.4%] w-[28.7%]">
+            {formik.values.request_info.location}
+          </p>
+          <p className="absolute top-[18%] left-[31.7%] w-[7%] text-center">
+            {formik.values.request_info.cost}
+          </p>
+          <p className="absolute top-[18%] left-[43.7%] w-[27%] text-center">
+            {formik.values.request_info.cost_des_th}
+          </p>
+          <i
+            className={classNames(
+              "fas fa-check absolute top-[22%] text-[16px]",
+              {
+                "left-[40.5%]": formik.values.activity_type === "hour_count",
+                "left-[9.8%]": formik.values.activity_type === "hour_not_count",
+                "opacity-0": formik.values.activity_type === "",
+              }
+            )}
+          ></i>
+          {formik.values.activity_type === "hour_count" && (
+            <Fragment>
+              <p className="absolute top-[23.35%] left-[80.7%] w-[4%] text-center">
+                {_.find(
+                  formik.values.sub_activity,
+                  (subActivity) => subActivity.sub_activity_id === "1"
+                )?.activity_hour || 0}
+              </p>
+              <p className="absolute top-[25.05%] left-[80.7%] w-[4%] text-center">
+                {_.find(
+                  formik.values.sub_activity,
+                  (subActivity) => subActivity.sub_activity_id === "2"
+                )?.activity_hour || 0}
+              </p>
+              <p className="absolute top-[26.8%] left-[80.7%] w-[4%] text-center">
+                {_.find(
+                  formik.values.sub_activity,
+                  (subActivity) => subActivity.sub_activity_id === "3"
+                )?.activity_hour || 0}
+              </p>
+              <p className="absolute top-[28.5%] left-[80.7%] w-[4%] text-center">
+                {_.find(
+                  formik.values.sub_activity,
+                  (subActivity) => subActivity.sub_activity_id === "4"
+                )?.activity_hour || 0}
+              </p>
+              <p className="absolute top-[30.3%] left-[80.7%] w-[4%] text-center">
+                {_.find(
+                  formik.values.sub_activity,
+                  (subActivity) => subActivity.sub_activity_id === "5"
+                )?.activity_hour || 0}
+              </p>
+              <p className="absolute top-[32%] left-[80.7%] w-[4%] text-center">
+                {_.find(
+                  formik.values.sub_activity,
+                  (subActivity) => subActivity.sub_activity_id === "6"
+                )?.activity_hour || 0}
+              </p>
+            </Fragment>
+          )}
+          <p className="absolute top-[80.3%] left-[74.5%] w-[14%] text-center">
+            {formik.values.form_info.contact}
+          </p>
         </div>
       )}
     </Observer>
